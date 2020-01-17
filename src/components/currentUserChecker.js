531d14e2dec7fa1b2f6f0cc189abc0c7ddccc6e0
Andrew Voytsehovski <anvoytsehovski@gmail.com>
2020-01-17 11:40:46 +0200

    use-callback-hook

--- src/components/currentUserChecker.js
+++ src/components/currentUserChecker.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import { useEffect, useContext } from 'react'
 
 import useFetch from '../hooks/useFetch'
 import { CurrentUserContext } from './../contexts/currentUser'
