ca01a8cfda69afcc81770af9a1428843ee0725de
Andrew Voytsehovski <anvoytsehovski@gmail.com>
2020-01-30 11:45:24 +0200

    implementing-tag-feed

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom'
 import GlobalFeed from './globalFeed'
 import Article from './article'
 import Authentication from './authentication'
+import TagFeed from './tagFeed'
 
 export default () => {
     return (
         <Switch>
             <Route path="/" component={ GlobalFeed } exact />
+            <Route path="/tags/:slug" component={ TagFeed } />
             <Route path="/login" component={ Authentication } />
             <Route path="/register" component={ Authentication } />
             <Route path="/articles/:slug" component={ Article } />
