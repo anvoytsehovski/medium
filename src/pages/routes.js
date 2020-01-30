a23281aa697a3984aec24863067420f7afbb0337
Andrew Voytsehovski <anvoytsehovski@gmail.com>
2020-01-30 12:41:46 +0200

    implementing-your-feed

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -5,11 +5,13 @@ import GlobalFeed from './globalFeed'
 import Article from './article'
 import Authentication from './authentication'
 import TagFeed from './tagFeed'
+import YourFeed from './yourFeed'
 
 export default () => {
     return (
         <Switch>
             <Route path="/" component={ GlobalFeed } exact />
+            <Route path="/feed" component={ YourFeed } />
             <Route path="/tags/:slug" component={ TagFeed } />
             <Route path="/login" component={ Authentication } />
             <Route path="/register" component={ Authentication } />
